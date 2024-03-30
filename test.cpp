0da49505d28398221612ceb3b5ef7a5ab161f1fb
WilbertFE <wilbertbernadi699@gmail.com>
2024-03-30 14:51:29 +0700

    10-operator-pembanding

--- test.cpp
+++ test.cpp
@@ -2,23 +2,16 @@
 using namespace std;
 
 int main (){
-    double L, a, t;
-
-    cout << "Menghitung luas segitiga\n===============================" << endl;
-    cout << "Masukkan alas \t= ";
+    int a, hasil;
+    cout << "Masukkan bilangan bulat = ";
     cin >> a;
-    cout << "Masukkan tinggi \t= ";
-    cin >> t;
 
-    L = (a * t) / 2;
+    hasil = a % 2;
 
-    cout << "Luas segitiga yang diketahui : " << endl;
-    cout << "alas\t=" << a << "cm" << endl;
-    cout << "tinggi\t=" << t << "cm" << endl;
-    cout << "adalah : " << endl;
-    cout << "L = (a x t) / 2";
-     
+    string kondisi = hasil == 1 ? "Ganjil" : "Genap";
+
+    cout << "angka " << a << " adalah bilangan " << kondisi;
+
 
     return 0;
 }
-
